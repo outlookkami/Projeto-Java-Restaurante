ef8c83ab1bbbeebb9d50d1036b761a4a3e090072
outlookkami <dkamillythauanny@gmail.com>
2025-12-23 17:25:34 -0300

    Docker compose + Entidade usuário (parcial - entendendo a lógica)

---
+++ src/main/java/com/projetoJavaRestaurante/demo/model/Usuario.java
@@ -0,0 +1,47 @@
+package com.projetoJavaRestaurante.demo.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private perfilUsuario perfil;
+
+    @Column(nullable = false)
+    private boolean ativo = true;
+
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao =  LocalDateTime.now();
+
+    @PrePersist
+    public void prePersist() {
+        this.dataCriacao = LocalDateTime.now();
+    }
+
+    //getters
+    public int getId() {return id;}
+    public String getNome() {return nome;}
+    public String getEmail() {return email;}
+    
+    //getters Lombok
+    @Getter
+
+}
